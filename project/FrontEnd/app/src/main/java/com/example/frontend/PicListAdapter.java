faa2ea2e14961622366f9543a00fba94412aef5d
z-c18 <z-c18@mails.tsinghua.edu.cn>
2022-05-14 16:15:47 +0800

    上传图文v1

--- project/FrontEnd/app/src/main/java/com/example/frontend/PicListAdapter.java
+++ project/FrontEnd/app/src/main/java/com/example/frontend/PicListAdapter.java
@@ -3,6 +3,7 @@ package com.example.frontend;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 import com.w4lle.library.NineGridAdapter;
 
+import java.io.File;
 import java.util.List;
 
 public class PicListAdapter extends NineGridAdapter {
@@ -26,7 +28,6 @@ public int getCount() {
 
 @Override
 public String getUrl(int position) {
-        Log.d("1111",getItem(position).toString());
         return getItem(position) == null ? null : getItem(position).toString();
         }
 
@@ -42,20 +43,14 @@ public long getItemId(int position) {
 
 @Override
 public View getView(int i, View view) {
-//        ImageView imageView = new ImageView(context);
-//        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT);
-//
-//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//        imageView.setBackgroundColor(Color.parseColor("#f5f5f5"));
-//        imageView.setLayoutParams(lp);
-//        Glide.with(context).load(getUrl(i)).into(imageView);
-//        return imageView;
         ImageView iv = new ImageView(context);
-        Log.d("234", "1");
         iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
         iv.setBackgroundColor(Color.parseColor("#f5f5f5"));
-        Picasso.with(context).load(getUrl(i)).placeholder(new ColorDrawable(Color.parseColor("#f5f5f5"))).into(iv);
+
+        Picasso.with(context)
+                .load( new File(getUrl(i))  )
+                .placeholder(new ColorDrawable(Color.parseColor("#f5f5f5")))
+                .into(iv);
         return iv;
         }
 }
--- project/FrontEnd/app/src/main/java/com/example/frontend/PublishPicActivity.java
+++ project/FrontEnd/app/src/main/java/com/example/frontend/PublishPicActivity.java
@@ -1,14 +1,22 @@
 package com.example.frontend;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
@@ -19,14 +27,45 @@ import com.squareup.picasso.Picasso;
 import com.w4lle.library.NineGridAdapter;
 import com.w4lle.library.NineGridlayout;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 import me.nereo.multi_image_selector.MultiImageSelectorActivity;
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class PublishPicActivity extends AppCompatActivity {
     private Button button_loadPic, button_loadPos, button_launch;
     private NineGridlayout nineGridlayout;
     static final int PHOTO_RETURN_CODE = 0;
+    private List<String> path;
+    private static final int handlerStateWarning = 0;
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == handlerStateWarning) {
+                String res = (String) msg.obj;
+                AlertDialog textTips = new AlertDialog.Builder(PublishPicActivity.this)
+                        .setTitle("Tips:")
+                        .setMessage(res)
+                        .create();
+                textTips.show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +74,7 @@ public class PublishPicActivity extends AppCompatActivity {
         button_loadPic = (Button) findViewById(R.id.add_pic);
         button_loadPos = (Button) findViewById(R.id.add_position_pic);
         button_launch = (Button) findViewById(R.id.publish_button_pic);
+        nineGridlayout = findViewById(R.id.iv_ngrid_layout);
 
         button_loadPic.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +91,15 @@ public class PublishPicActivity extends AppCompatActivity {
                 startActivityForResult(intent, PHOTO_RETURN_CODE);
             }
         });
+
+        button_launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String requestUrl = "http://43.138.84.226:8080/publish/picture";
+                PublishPicActivity.MyThreadPhoto myThread = new PublishPicActivity.MyThreadPhoto(requestUrl);// TO DO
+                myThread.start();// TO DO
+            }
+        });
     }
 
     @Override
@@ -59,13 +108,65 @@ public class PublishPicActivity extends AppCompatActivity {
         if(requestCode == PHOTO_RETURN_CODE){
             if(resultCode == RESULT_OK){
                 // Get the result list of select image paths
-                List<String> path = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
-                nineGridlayout = findViewById(R.id.iv_ngrid_layout);
+                path = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
+
                 PicListAdapter adapter = new PicListAdapter(PublishPicActivity.this, path);
-                nineGridlayout.setVisibility(View.GONE);
                 nineGridlayout.setAdapter(adapter);
-                Log.d("111", path.get(0));
+
                 // do your logic ....
+            }
+        }
+    }
+
+    class MyThreadPhoto extends Thread{
+        private String requestUrl;
+        MyThreadPhoto(String request){
+            requestUrl = request;
+        }
+        @Override
+        public void run() {
+            try {
+                OkHttpClient client = new OkHttpClient();
+//                file = new File(filepath);
+                MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+                SharedPreferences sharedPreferences = getSharedPreferences("login",MODE_PRIVATE);
+                String cookie = sharedPreferences.getString("session","");
+
+                MultipartBody.Builder builder = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM);
+                for(int i = 0; i < path.size(); ++i){
+                    File f = new File(path.get(i));
+                    RequestBody fileBody = RequestBody.create(MEDIA_TYPE_PNG, f);
+                    builder.addFormDataPart("pic_"+(i+1), f.getName(), fileBody);
+                }
+                RequestBody requestBody = builder
+                        .addFormDataPart("title", "test")
+                        .addFormDataPart("content", "test")
+                        .addFormDataPart("location", "test")
+                        .addFormDataPart("pic_num", "" + path.size())
+                        .build();
+                // create a file to write bitmap data
+
+
+                Request request = new Request.Builder()
+                        .url(requestUrl)
+                        .post(requestBody)
+                        .addHeader("cookie",cookie)
+                        .build();
+
+                Call call = client.newCall(request);
+                Response response = call.execute();
+                if (response.isSuccessful()) {
+                    Message msg = handler.obtainMessage(handlerStateWarning);
+                    msg.obj = Objects.requireNonNull(response.body()).string();
+                    handler.sendMessage(msg);
+                } else {
+                    throw new IOException("Unexpected code " + response);
+                }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
             }
         }
     }
