7551362f046ade166b5dfb27c6bf0325a02a1033
Matsu-0 <1147172850@qq.com>
2022-05-16 00:32:42 +0800

    添加了GPS位置信息，待完成组件显示

--- project/FrontEnd/app/src/main/java/com/example/frontend/PublishPicActivity.java
+++ project/FrontEnd/app/src/main/java/com/example/frontend/PublishPicActivity.java
@@ -1,11 +1,17 @@
 package com.example.frontend;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 
@@ -14,18 +20,26 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.Image;
 
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 import com.w4lle.library.NineGridlayout;
 
 import java.io.File;
@@ -47,12 +61,23 @@ import okhttp3.Response;
 
 public class PublishPicActivity extends AppCompatActivity {
     private Button button_loadPic, button_loadPos, button_launch, button_takePhoto, button_clearPhoto;
+    private EditText edit_title, edit_detail;
     private NineGridlayout nineGridlayout;
+    private String title, content, location = null;
+    private AlertDialog textTips;
+
     static final int PHOTO_RETURN_CODE = 0;
     static final int TAKE_PHOTO_RETURN_CODE = 1;
+    private static final int PERMISSION_APPLY = 2;
+    private static final int OPEN_GPS_CODE = 1001;
+    private static final int LOCATION_CODE = 1000;
+    private static final String TAG = "PublishPic";
+
     private List<String> path = new ArrayList<>();
     private String currentPhotoPath;
     private static final int handlerStateWarning = 0;
+    LocationManager locationManager;
+
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler(){
         @Override
@@ -60,17 +85,26 @@ public class PublishPicActivity extends AppCompatActivity {
             super.handleMessage(msg);
             if (msg.what == handlerStateWarning) {
                 String res = (String) msg.obj;
-                AlertDialog textTips = new AlertDialog.Builder(PublishPicActivity.this)
+                textTips = new AlertDialog.Builder(PublishPicActivity.this)
                         .setTitle("Tips:")
                         .setMessage(res)
                         .create();
                 textTips.show();
-                if (res == "发布成功"){
+                Log.d("res", "!"+res);
+                if (res.equals("发布成功")){
+                    Log.d("状态","发布成功");
                     finish();
                 }
             }
         }
     };
+
+    protected void onDestroy() {
+        if (textTips != null) {
+            textTips.dismiss();
+        }
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +115,8 @@ public class PublishPicActivity extends AppCompatActivity {
         button_launch = (Button) findViewById(R.id.publish_button_pic);
         button_takePhoto = (Button) findViewById(R.id.take_photo);
         button_clearPhoto = (Button) findViewById(R.id.clear_all_pic);
+        edit_title = (EditText) findViewById(R.id.publish_pic_title);
+        edit_detail = (EditText) findViewById(R.id.publish_pic_detail);
         nineGridlayout = findViewById(R.id.iv_ngrid_layout);
 
         button_loadPic.setOnClickListener(new View.OnClickListener() {
@@ -116,6 +152,16 @@ public class PublishPicActivity extends AppCompatActivity {
                 nineGridlayout.setAdapter(adapter);
             }
         });
+
+        button_loadPos.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                location = getProvince();
+                Log.d("location", location);
+            }
+        });
+
 
         button_takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -212,12 +258,41 @@ public class PublishPicActivity extends AppCompatActivity {
                 }
 
                 // TO DO：修改参数并添加判断
-                RequestBody requestBody = builder
-                        .addFormDataPart("title", "test")
-                        .addFormDataPart("content", "test")
-                        .addFormDataPart("location", "test")
-                        .addFormDataPart("pic_num", "" + path.size())
-                        .build();
+                title = edit_title.getText().toString();
+
+                if (title == null || title.length() == 0){
+                    Message msg = handler.obtainMessage(handlerStateWarning);
+                    msg.obj = "标题不能为空";
+                    handler.sendMessage(msg);
+                    throw new IOException("Title is null");
+                }
+
+                content = edit_detail.getText().toString();
+
+                if (content == null || content.length() == 0){
+                    Message msg = handler.obtainMessage(handlerStateWarning);
+                    msg.obj = "内容不能为空";
+                    handler.sendMessage(msg);
+                    throw new IOException("Content is null");
+                }
+
+                RequestBody requestBody;
+
+                if (location == null || location.length() == 0){
+                    requestBody = builder
+                            .addFormDataPart("title", title)
+                            .addFormDataPart("content", content)
+                            .addFormDataPart("pic_num", "" + path.size())
+                            .build();
+                }
+                else {
+                    requestBody = builder
+                            .addFormDataPart("title", title)
+                            .addFormDataPart("content", content)
+                            .addFormDataPart("location", location)
+                            .addFormDataPart("pic_num", "" + path.size())
+                            .build();
+                }
                 // create a file to write bitmap data
 
 
@@ -242,5 +317,123 @@ public class PublishPicActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public String getProvince() {
+
+        // 动态权限申请
+        if (Build.VERSION.SDK_INT >= 23) {
+            if(ContextCompat.checkSelfPermission(PublishPicActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // 申请读写内存卡内容的权限
+                Log.d("是否授权","false1");
+                ActivityCompat.requestPermissions(PublishPicActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSION_APPLY);
+            }
+        }
+
+        // 动态权限申请
+        if (Build.VERSION.SDK_INT >= 23) {
+            if(ContextCompat.checkSelfPermission(PublishPicActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // 申请读写内存卡内容的权限
+                Log.d("是否授权","false2");
+                ActivityCompat.requestPermissions(PublishPicActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSION_APPLY);
+            }
+        }
+
+        Log.i("GPS ", "getProvince");
+        locationManager = (LocationManager) PublishPicActivity.this.getSystemService(Context.LOCATION_SERVICE);        // 默认Android GPS定位实例
+
+        Location location = null;
+        String p = "";
+        // 是否已经授权
+        if (PublishPicActivity.this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            //判断GPS是否开启，没有开启，则开启
+            //Log.d("是否授权","true");
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                //Log.d("是否开启","false");
+                //跳转到手机打开GPS页面
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                PublishPicActivity.this.startActivity(intent);
+            } else {
+                Criteria criteria = new Criteria();
+                criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                String provider = locationManager.getBestProvider(criteria, true);
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        2000, 8, new LocationListener() {
+
+                            // Provider的状态在可用、暂时不可用和无服务三个状态直接切换时触发此函数
+                            @Override
+                            public void onStatusChanged(String provider, int status, Bundle extras) {
+                                Log.d(TAG, "onStatusChanged");
+                            }
+
+                            // Provider被enable时触发此函数，比如GPS被打开
+                            @Override
+                            public void onProviderEnabled(String provider) {
+                                Log.d(TAG, "onProviderEnabled");
+
+                            }
+
+                            // Provider被disable时触发此函数，比如GPS被关闭
+                            @Override
+                            public void onProviderDisabled(String provider) {
+                                Log.d(TAG, "onProviderDisabled");
+
+                            }
+
+                            //当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+                            @Override
+                            public void onLocationChanged(Location location) {
+                                Log.d(TAG, String.format("location: longitude: %f, latitude: %f", location.getLongitude(),
+                                        location.getLatitude()));
+                                //更新位置信息
+                            }
+                        });
+                location = locationManager.getLastKnownLocation(provider);
+            }
+
+            if (location != null) {
+                Log.i("GPS ", "获取位置信息成功");
+                Log.i("GPS ", "经度：" + location.getLatitude());
+                Log.i("GPS ", "纬度：" + location.getLongitude());
+
+                // 获取地址信息
+                p = getAddress(location.getLatitude(), location.getLongitude());
+                Log.i("GPS ", "location：" + p);
+            } else {
+                Log.i("GPS ", "获取位置信息失败，请检查是否开启GPS,是否授权");
+            }
+        }
+        return p;
+    }
+
+    /*
+     * 根据经度纬度 获取国家，省份
+     * */
+    public String getAddress(double latitude, double longitude) {
+        String cityName = "";
+        List<Address> addList = null;
+        Geocoder ge = new Geocoder(PublishPicActivity.this);
+        try {
+            addList = ge.getFromLocation(latitude, longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addList != null && addList.size() > 0) {
+            for (int i = 0; i < addList.size(); i++) {
+                Address ad = addList.get(i);
+                cityName += ad.getCountryName() + " " + ad.getLocality();
+            }
+        }
+        return cityName;
     }
 }
